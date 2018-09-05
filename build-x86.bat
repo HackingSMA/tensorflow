aaf5dc5c3eff0f15fadec029ccecc98bdb9b2e07
Shea Ako <shea.ako@tobiidynavox.com>
2018-09-05 13:32:45 -0500

    testing

--- build-x86.bat
+++ build-x86.bat
@@ -3,6 +3,6 @@
 mkdir tensorflow\contrib\cmake\build
 cd tensorflow\contrib\cmake\build
 
-cmake .. -T host=x64 -DCMAKE_BUILD_TYPE=Release -Dtensorflow_BUILD_PYTHON_BINDINGS=OFF -Dtensorflow_ENABLE_GRPC_SUPPORT=OFF -Dtensorflow_BUILD_SHARED_LIB=ON -Dtensorflow_VERBOSE=ON -Dtensorflow_BUILD_CONTRIB_KERNELS=OFF -Dtensorflow_OPTIMIZE_FOR_NATIVE_ARCH=OFF -Dtensorflow_WIN_CPU_SIMD_OPTIONS=OFF 
+cmake .. -T host=x64 -DCMAKE_BUILD_TYPE=Release -Dtensorflow_BUILD_PYTHON_BINDINGS=OFF -Dtensorflow_ENABLE_GRPC_SUPPORT=OFF -Dtensorflow_BUILD_SHARED_LIB=ON -Dtensorflow_VERBOSE=ON -Dtensorflow_BUILD_CONTRIB_KERNELS=OFF -Dtensorflow_WIN_CPU_SIMD_OPTIONS=/arch:AVX
 
 MSBuild /p:Configuration=Release tf_tutorials_example_trainer.vcxproj
