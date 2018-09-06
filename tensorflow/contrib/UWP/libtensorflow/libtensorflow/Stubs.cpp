142e377ef00fbddc4546608f42a1ee9565e13927
Shea Ako <shea.ako@tobiidynavox.com>
2018-09-05 19:26:49 -0500

    check

---
+++ tensorflow/contrib/UWP/libtensorflow/libtensorflow/Stubs.cpp
@@ -0,0 +1,107 @@
+#include "pch.h"
+
+//Kernel32.lib
+
+//
+//
+//EXTERN_C HANDLE WINAPI CreateFileA(LPCSTR lpFileName, DWORD dwDesiredAccess,
+//	DWORD dwShareMode,
+//	LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+//	DWORD dwCreationDisposition,
+//	DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+//{
+//  return NULL;
+//}
+//
+//
+//
+//
+//
+//EXTERN_C HMODULE WINAPI LoadLibraryA(_In_ LPCTSTR lpFileName)
+//{
+//	return NULL;
+//}
+//
+//EXTERN_C HMODULE WINAPI LoadLibraryW(_In_ LPCTSTR lpFileName)
+//{ 
+//	return NULL; 
+//}
+//
+//EXTERN_C HMODULE WINAPI GetModuleHandleA(_In_opt_ LPCTSTR lpModuleName)
+//{
+//  return NULL;
+//}
+//
+//
+////EXTERN_C DWORD WINAPI GetCurrentDirectory(DWORD nBufferLength, LPTSTR lpBuffer)
+////{
+////  return 0;
+////}
+//
+//EXTERN_C BOOL WINAPI HeapValidate(HANDLE hHeap, DWORD dwFlags, LPCVOID lpMem)
+//{
+//  return false;
+//}
+
+
+
+
+//EXTERN_C LPVOID WINAPI MapViewOfFile(_In_ HANDLE hFileMappingObject,
+//                                     _In_ DWORD dwDesiredAccess,
+//                                     _In_ DWORD dwFileOffsetHigh,
+//                                     _In_ DWORD dwFileOffsetLow,
+//                                     _In_ SIZE_T dwNumberOfBytesToMap);
+//{
+//  return NULL;
+//}
+
+// EXTERN_C HANDLE WINAPI CreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess,
+//	DWORD dwShareMode,
+//	LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+//	DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes,
+//	HANDLE hTemplateFile)
+//{
+//  return NULL;
+//}
+
+// EXTERN_C LPVOID WINAPI MapViewOfFileEx(_In_ HANDLE hFileMappingObject,
+//	_In_ DWORD dwDesiredAccess,
+//	_In_ DWORD dwFileOffsetHigh,
+//	_In_ DWORD dwFileOffsetLow,
+//	_In_ SIZE_T dwNumberOfBytesToMap,
+//	_In_opt_ LPVOID lpBaseAddress)
+//{
+//  return NULL;
+//}
+
+
+//EXTERN_C HANDLE WINAPI
+//CreateFileMappingA(HANDLE hFile, LPSECURITY_ATTRIBUTES lpFileMappingAttributes,
+//                   DWORD flProtect, DWORD dwMaximumSizeHigh,
+//                   DWORD dwMaximumSizeLow, LPCSTR lpName) {
+//  return NULL;
+//}
+
+//EXTERN_C BOOL WINAPI GetFileInformationByHandle(HANDLE hFile,
+//                                                void* lpFileInformation) {
+//  return false;
+//}
+
+//EXTERN_C DWORD WINAPI GetFileSize(HANDLE hFile, LPDWORD lpFileSizeHigh) {
+//  return 0;
+//}
+
+// EXTERN_C HMODULE WINAPI LoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile,
+// DWORD dwFlags)
+//{
+//  return NULL;
+//}
+
+// EXTERN_C HMODULE WINAPI GetModuleHandleW(_In_opt_ LPCTSTR lpModuleName) {
+//  return NULL;
+//}
+
+// EXTERN_C DWORD WINAPI GetVersion(void)
+//{
+//	return 0;
+//}
